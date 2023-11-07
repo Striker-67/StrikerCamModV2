4a67185012f3c7bbefdebc8d5212e6a6e60bd943
striker67 <86896895+FURY909@users.noreply.github.com>
2023-11-06 20:54:39 -0600

    first commit

---
+++ strikercammod/strikercammod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace strikercammod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.striker.gorillatag.strikercammod";
+        public const string Name = "strikercammod";
+        public const string Version = "1.0.0";
+    }
+}
